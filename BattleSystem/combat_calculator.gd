1b889e45f57e598e96ca74c448c7f10ae5455f1d
David Barner <dibarner@gmail.com>
2023-11-24 14:35:21 -0800

    Update combat_calculator.gd
    functions were in wrong class; put them in the correct one

--- BattleSystem/combat_calculator.gd
+++ BattleSystem/combat_calculator.gd
@@ -9,9 +9,9 @@ func ability_performed(_actor: BattleActor, _ability: Ability, _targets: Array[B
 	var report: CombatActivityReport = CombatActivityReport.new(_actor, _ability, _targets)
 	
 	# calculator ability effect
-	var hitChance: float = CombatActivityReport.get_hit_chance(_actor.dex, _targets[0].dex)
-	var criticalChance: float = CombatActivityReport.get_critical_chance(_actor.dex);
-	var criticalStrength: float = CombatActivityReport.get_critical_strength(_actor.str);
+	var hitChance: float = CombatCalculator.get_hit_chance(_actor.dex, _targets[0].dex)
+	var criticalChance: float = CombatCalculator.get_critical_chance(_actor.dex);
+	var criticalStrength: float = CombatCalculator.get_critical_strength(_actor.str);
 	report.hitSuccess = rng.randf_range(0, 1) <= hitChance
 	report.critSuccess = rng.randf_range(0, 1) <= criticalChance
 	report.totalDamage = (report.abilityData.damage + report.attacker.str) - _targets[0].con
@@ -34,6 +34,18 @@ func ability_performed(_actor: BattleActor, _ability: Ability, _targets: Array[B
 
 	# send out combat report
 	combat_activity_report_generated.emit(report)
+
+static func get_hit_chance(_attacker: float, _defender: float) -> float:
+	# TODO: this can divide by 0
+	return _attacker / (_attacker + _defender * 0.66) # 0-100%
+
+
+static func get_critical_chance(_attacker: float) -> float:
+	return .25 * _attacker / 100 # 1-100 => 1-25%
+
+
+static func get_critical_strength(_attacker: float) -> float:
+	return .1 + pow(1.01, _attacker) # min: 1.11
 
 
 class CombatActivityReport:
@@ -86,15 +98,3 @@ class CombatActivityReport:
 		
 		self.defender = _targets[0]
 
-
-	static func get_hit_chance(_attacker: float, _defender: float) -> float:
-		# TODO: this can divide by 0
-		return _attacker / (_attacker + _defender * 0.66) # 0-100%
-
-
-	static func get_critical_chance(_attacker: float) -> float:
-		return .25 * _attacker / 100 # 1-100 => 1-25%
-
-
-	static func get_critical_strength(_attacker: float) -> float:
-		return .1 + pow(1.01, _attacker) # min: 1.11
